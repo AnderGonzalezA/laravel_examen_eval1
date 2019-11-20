777b3ae72cf2d21c47b31d9e0849816980dc0d1c
“ajuanenama” <ajuanenama@zubirimanteo.com>
2019-11-20 08:54:45 +0100

    para hacer composer update

--- resources/views/git.blade.php
+++ resources/views/git.blade.php
@@ -3,8 +3,13 @@
 @section('title', 'UD5. ORM Eloquent')
 
 @section('content')
+Introduce el resultado del ejercicio de git (ejercicio 1).
 
-  Introduce el resultado del ejercicio de git (ejercicio 2).
+git checkout master
+git merge tarea1
 
+Introduce el resultado del ejercicio de git (ejercicio 2).
+
+El modelo-vista-controlador es una estructura para desarrollar aplicaciónes, que consiste en tener separados los tres siguientes componentes que lo forman: el modelo, que es la parte que se encarga de los datos; el controlador, que se encarga de las los eventos y las comunicaciones; y la vista, que se encarga de la presentación de la aplicación. Entre las ventajas de organizar un proyecto mediante esta estructura se encuentra una ventaja clave: separación de conceptos (si modificamos por ejemplo la parte del modelo, esto no afecta directamente a la parte del controlador ni de la vista).
 
 @endsection
